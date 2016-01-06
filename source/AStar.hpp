4fd2d081d6cb8a8936bfc673b4ced7da1e9f3797
Damian Barczyński <daan.net@wp.eu>
2016-01-06 18:19:44 +0100

    Added license note.

--- source/AStar.hpp
+++ source/AStar.hpp
@@ -1,3 +1,8 @@
+/*
+    Copyright (c) 2015, Damian Barczynski <daan.net@wp.eu>
+    Following tool is licensed under the terms and conditions of the ISC license.
+    For more information visit https://opensource.org/licenses/ISC.
+*/
 #ifndef __ASTAR_HPP_8F637DB91972F6C878D41D63F7E7214F__
 #define __ASTAR_HPP_8F637DB91972F6C878D41D63F7E7214F__
 
